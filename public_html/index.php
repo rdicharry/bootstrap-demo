11dd56be396f04f99d9e2a08950328caa6bead14
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-03 13:30:31 -0600

    added navbar

--- public_html/index.php
+++ public_html/index.php
@@ -1,49 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8"/>
+<html lant="en">
+<head>
+	<?php require_once("lib/head-utils.php"); ?>
+</head>
 
-		<!-- Tell IE not to use compatibility mode, rather, use the highest available version
-		this helps avoid some "IE Compatibility Mode" bugs -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-		<!-- enable responsive behavior -->
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-		<!-- jQuery -->
-		<!-- Bootstrap's javascript requires jQuery, so it needs to be loaded first -->
-		<!-- source: cdnjs.com - choose minified version-->
-		<script source="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
-
-		<!-- copy and past from www.getBootstrap.com/getting-started/ BootstrapCDN -->
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
-		<!-- my custom css file -->
-		<link rel="stylesheet" href="css/styles.css" type="text/css">
-
-		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		<!-- Note: this conditional comment is slated to be removed from Bootstrap4 -->
-
-
-
-		<title>Bootstrap Demo</title>
-
-	</head>
 	<body>
-		Hai, Bootstrap! :D
+
+		<div class="container">
+
+			<!-- Heading -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Welcome!</h1>
+				</div>
+			</div>
+			<!-- first content area -->
+			<div class="row">
+				<div class="col-md-6">
+					Text Area 1
+				</div>
+				<div class="col-md-6">
+					Text Area 2
+				</div>
+			</div>
+
+			<!-- second content area -->
+			<div class="row">
+				<div class="col-md-3">
+					Content1
+				</div>
+				<div class="col-md-3">
+					Content2
+				</div>
+				<div class="col-md-3">
+					Content3
+				</div>
+				<div class="col-md-3">
+					Content4
+				</div>
+			</div>
+
+			<div class="row text-center">
+				<div class="col-xs-12">
+					<p><a href="demo-2.php">Next Page >>></a></p>
+				</div>
+			</div>
+
+		</div>
 	</body>
 </html>
---
+++ public_html/lib/head-utils.php
@@ -0,0 +1,46 @@
+
+
+	<meta charset="UTF-8"/>
+
+	<!-- Tell IE not to use compatibility mode, rather, use the highest available version
+	this helps avoid some "IE Compatibility Mode" bugs -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<!-- enable responsive behavior -->
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<!-- jQuery -->
+	<!-- Bootstrap's javascript requires jQuery, so it needs to be loaded first -->
+	<!-- source: cdnjs.com - choose minified version-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
+
+
+	<!-- copy and past from www.getBootstrap.com/getting-started/ BootstrapCDN -->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- my custom css file -->
+	<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+
+	<!-- Latest compiled and minified JavaScript (from getBootstrap.com)-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+
+
+	<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<!-- Note: this conditional comment is slated to be removed from Bootstrap4 -->
+
+
+
+	<title>Bootstrap Demo</title>
